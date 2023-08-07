17a5f90052fe0db2a248610ab289b5b185731163
Cecil Tantay <cjbtantay@gmail.com>
2023-08-06 22:13:11 -0700

    fix terraform state

--- aws/iam/environments/production/main.tf
+++ aws/iam/environments/production/main.tf
@@ -32,27 +32,3 @@ resource "aws_iam_role" "serverless-role" {
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
   description = "Serverless role for production"
 }
-
-resource "aws_iam_policy" "serverless_policy_1" {
-  name        = "serverless_policy"
-  description = "A policy for Serverless services"
-  policy      = data.aws_iam_policy_document.serverless_policy_part_1.json
-}
-
-resource "aws_iam_policy" "serverless_policy_2" {
-  name        = "serverless_policy_2"
-  description = "A policy for Serverless services"
-  policy      = data.aws_iam_policy_document.permissions_policy_part_2.json
-}
-
-resource "aws_iam_policy_attachment" "serverless-policy_attachment" {
-  name       = "serverless-policy-attachment"
-  roles      = [aws_iam_role.serverless-role.name]
-  policy_arn = aws_iam_policy.serverless_policy_1.arn
-}
-
-resource "aws_iam_policy_attachment" "serverless-policy_attachment_2" {
-  name       = "serverless-policy-attachment_2"
-  roles      = [aws_iam_role.serverless-role.name]
-  policy_arn = aws_iam_policy.serverless_policy_2.arn
-}
