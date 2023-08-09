d25bc32ec9885dcba8290e2eee8fc2bec7255a48
Cecil Tantay <cjbtantay@gmail.com>
2023-08-09 03:16:57 -0700

    delete stuff

--- aws/iam/environments/staging/main.tf
+++ aws/iam/environments/staging/main.tf
@@ -25,34 +25,10 @@ provider "aws" {
   }
 }
 
-# Part 1: Create a role for serverless
+# # Part 1: Create a role for serverless
 resource "aws_iam_role" "serverless-role" {
   name = "ServerlessStagingRole"
 
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
   description = "Serverless role for staging"
-}
-
-resource "aws_iam_policy" "serverless_policy_1" {
-  name        = "serverless_policy"
-  description = "A policy for Serverless services"
-  policy      = data.aws_iam_policy_document.serverless_policy_part_1.json
-}
-
-resource "aws_iam_policy" "serverless_policy_2" {
-  name        = "serverless_policy_2"
-  description = "A policy for Serverless services"
-  policy      = data.aws_iam_policy_document.permissions_policy_part_2.json
-}
-
-resource "aws_iam_policy_attachment" "serverless-policy_attachment" {
-  name       = "serverless-policy-attachment"
-  roles      = [aws_iam_role.serverless-role.name]
-  policy_arn = aws_iam_policy.serverless_policy_1.arn
-}
-
-resource "aws_iam_policy_attachment" "serverless-policy_attachment_2" {
-  name       = "serverless-policy-attachment_2"
-  roles      = [aws_iam_role.serverless-role.name]
-  policy_arn = aws_iam_policy.serverless_policy_2.arn
 }
